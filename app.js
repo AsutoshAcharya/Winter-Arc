93ae3f24f0accd06bb8a33d1d2240654143257f3
Asutosh Acharya <asutosh246@gmail.com>
2023-09-28 19:38:01 +0530

    playing around

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-const http=require("http");
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<h1>Hello From Server.</h1>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">submit</button></form>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
-const server=http.createServer((req,res)=>{
-    console.log(req);
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.writeHead(302, { Location: "/" });
+    return res.end();
+  }
+  //   res.end();
 });
 
 server.listen(3000);
