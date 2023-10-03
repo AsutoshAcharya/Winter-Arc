110cea801551b99bfdbf871e5e35b9185b3100cf
Asutosh Acharya <asutosh246@gmail.com>
2023-10-03 21:17:19 +0530

    Limiting middleware executions

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //it executed for every incoming request
-app.use((req, res, next) => {
-  console.log("in middle ware");
-  next(); //allows the req to continue into next moddleware
+// app.use((req, res, next) => {
+//   console.log("in middle ware");
+//   next(); //allows the req to continue into next moddleware
+// });
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name ="title" /><button type="submit">Add Product</button></form>'
+  );
+});
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-app.use((req, res, next) => {
-  console.log("in another ware");
+app.use("/", (req, res, next) => {
+  //   console.log("in another ware");
+  res.send(
+    '<h1 style="background-color:red;color:cyan;width:50%">Hello from express </h1>'
+  );
 });
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
